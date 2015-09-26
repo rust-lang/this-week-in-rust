01283ce00ad62c3a373a82eac8478282652ab175
Corey Richardson <corey@octayn.net>
2015-09-26 15:49:16 -0400

    Use new deploy key

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 . ./bin/activate
 ./bin/pelican content -s pelicanconf.py -t themes/pelican-elegant-1.3 || exit
 if [ "$REALLY_DEPLOY" = "1" ]; then
-    rsync -razvP --delete-after output/ cmr@octayn.net:twir
+    rsync -e "ssh -i ~/.ssh/twir_deploy_key" -razvP --delete-after output/ twir@octayn.net:production
 fi
