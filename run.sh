7c48368c23ec7c7e8b7b957fa819d095a7be0e9b
Brian Anderson <banderson@mozilla.com>
2015-09-28 11:32:15 -0700

    Use HOME instead of ~

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 . ./bin/activate
 ./bin/pelican content -s pelicanconf.py -t themes/pelican-elegant-1.3 || exit
 if [ "$REALLY_DEPLOY" = "1" ]; then
-    rsync -e "ssh -i ~/.ssh/twir_deploy_key" -razvP --delete-after output/ twir@octayn.net:production
+    rsync -e "ssh -i $HOME/.ssh/twir_deploy_key" -razvP --delete-after output/ twir@octayn.net:production
 fi
