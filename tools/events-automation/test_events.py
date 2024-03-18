b7adf5dcacf708082749429fa11f21decf4b44d3
Hannah Cotterell <143161783+HanCotterell@users.noreply.github.com>
2024-03-17 18:52:36 -0700

    Update test_events.py
    Altered a few test event parameters to better test the sorting functions. Also changed the datetime data, so it only holds a date value rather than date and time value.

--- tools/events-automation/test_events.py
+++ tools/events-automation/test_events.py
@@ -1,11 +1,11 @@
 from event import Event
 from typing import List
-from datetime import datetime
+from datetime import date, timedelta
 
 
 def get_test_events() -> List[Event]:
-  return [Event(name="Test Event 1", location="Earth", date=datetime.now(), url="website1.com", virtual=False, organizerName="Test Organizer", maybeSpam=False), 
-          Event(name="Test Event 2", location="Earth", date=datetime.now(), url="website2.com", virtual=False, organizerName="Test Organizer", maybeSpam=False),
-          Event(name="Test Event 3", location="Earth", date=datetime.now(), url="website3.com", virtual=False, organizerName="Test Organizer", maybeSpam=False),
-          Event(name="Test Event 4", location="Earth", date=datetime.now(), url="website4.com", virtual=False, organizerName="Test Organizer", maybeSpam=False)
+  return [Event(name="Test Event 1", location="Earth", date=date.today() + timedelta(days=2), url="website1.com", virtual=True, organizerName="Test Organizer", maybeSpam=False), 
+          Event(name="Test Event 2", location="Earth", date=date.today() - timedelta(days=2), url="website2.com", virtual=False, organizerName="Test Organizer", maybeSpam=False),
+          Event(name="Test Event 3", location="Earth", date=date.today(), url="website3.com", virtual=True, organizerName="Test Organizer", maybeSpam=False),
+          Event(name="Test Event 4", location="Moon", date=date.today(), url="website4.com", virtual=False, organizerName="Test Organizer", maybeSpam=False)
           ]
